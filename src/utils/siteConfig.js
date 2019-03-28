a97d1dc98824ef77186385836fed06dc17bee2a0
Ken <kchau@microsoft.com>
2019-03-27 22:42:41 -0700

    fix feed url

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-    siteUrl: `crashtestdev.com`, // Site domain. Do not include a trailing slash!
+  siteUrl: `https://crashtestdev.com`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+  postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Crash test developer`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Ken Chau's blog about crash testing different developer things`, // This allows an alternative site description for meta data for pages.
+  siteTitleMeta: `Crash test developer`, // This allows an alternative site title for meta data for pages.
+  siteDescriptionMeta: `Ken Chau's blog about crash testing different developer things`, // This allows an alternative site description for meta data for pages.
 
-    shareImageWidth: 1000, // Change to the width of your default share image
-    shareImageHeight: 523, // Change to the height of your default share image
+  shareImageWidth: 1000, // Change to the width of your default share image
+  shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `CrashTestDev`, // Used for App manifest e.g. Mobile Home Screen
-    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
-    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
-    themeColor: `#15171A` // Used for Offline Manifest
+  shortTitle: `CrashTestDev`, // Used for App manifest e.g. Mobile Home Screen
+  siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
+  backgroundColor: `#e9e9e9`, // Used for Offline Manifest
+  themeColor: `#15171A` // Used for Offline Manifest
 };
